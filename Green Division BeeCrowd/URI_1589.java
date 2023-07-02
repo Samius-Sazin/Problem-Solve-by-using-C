6c232338352315cdbb825edad1f7bd721ef71d12
Samius Sazin <68127599+Samius-Sazin@users.noreply.github.com>
2023-07-03 00:57:42 +0600

    Add files via upload

---
+++ Green Division BeeCrowd/URI_1589.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class URI_1589 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int r1 = sc.nextInt();
+            int r2 = sc.nextInt();
+            System.out.println(r1+r2);
+        }
+        sc.close();
+    }
+}
