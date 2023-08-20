38a15991cf3c1b3c8eebc33ed41e1ac614b07f9b
Samius Sazin <68127599+Samius-Sazin@users.noreply.github.com>
2023-08-20 17:38:56 +0600

    Update A.cpp

--- CodeForce/E_153_div2/A.cpp
+++ CodeForce/E_153_div2/A.cpp
@@ -1,42 +1,93 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int t; cin >> t;
 
-    while(t--){
-        string str; cin >> str;
+//1st way to solve :
 
-        if(str == "()"){
-            cout << "NO\n";
-            continue;
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        string str;
+        cin >> str;
+
+        string a = "", b = "";
+
+        for (int i = 0; i < str.size(); i++)
+        {
+            a += "()";
+            b += "(";
+        }
+        for (int i = 0; i < str.size(); i++)
+        {
+            b += ")";
         }
 
-        int f = 0;
-        for(int i=0; i<str.size(); i++){
-            if(str[i] == str[i+1]){
-                f = 1;
-            }
+        if (a.find(str) == string::npos)
+        { // if condition is true that means str is not available in a string
+            cout << "YES\n"
+                 << a << endl;
         }
 
-        cout << "YES\n";
-
-        if(f == 1){
-            for(int i=0; i<str.size(); i++){
-                cout << "()";
-            }
-            cout << endl;
+        else if (b.find(str) == string::npos)
+        {
+            cout << "YES\n"
+                 << b << endl;
         }
 
-        else{
-            for(int i=0; i<str.size(); i++){
-                cout << "(";
-            }
-            for(int i=0; i<str.size(); i++){
-                cout << ")";
-            }
-            cout << endl;
+        else
+        {
+            cout << "NO" << endl;
         }
     }
 
     return 0;
 }
+
+
+
+
+// Another way
+
+// int main()
+// {
+//     int t;
+//     cin >> t;
+
+//     while(t--){
+//         if(str == "()"){
+//             cout << "NO\n";
+//             continue;
+//         }
+
+//         int f = 0;
+//         for(int i=0; i<str.size(); i++){
+//             if(str[i] == str[i+1]){
+//                 f = 1;
+//             }
+//         }
+
+//         cout << "YES\n";
+
+//         if(f == 1){
+//             for(int i=0; i<str.size(); i++){
+//                 cout << "()";
+//             }
+//             cout << endl;
+//         }
+
+//         else{
+//             for(int i=0; i<str.size(); i++){
+//                 cout << "(";
+//             }
+//             for(int i=0; i<str.size(); i++){
+//                 cout << ")";
+//             }
+//             cout << endl;
+//         }
+//     }
+
+//     return 0;
+// }
