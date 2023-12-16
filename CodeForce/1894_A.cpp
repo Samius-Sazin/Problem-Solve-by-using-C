9f7da721d9eeb6e7b4a9e8822a3bab34e23f5c07
Samius Sazin <68127599+Samius-Sazin@users.noreply.github.com>
2023-12-16 16:53:49 +0600

    Add files via upload

---
+++ CodeForce/1894_A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n; cin >> n;
+    string s; cin >> s;
+    cout << s[s.length()-1] << endl;
+}
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+}
