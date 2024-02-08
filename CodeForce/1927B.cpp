d12d74e3711cde4951c0e9dcc4e098e037075136
Samius Sazin <68127599+Samius-Sazin@users.noreply.github.com>
2024-02-08 18:07:11 +0600

    Update 1927B.cpp

--- CodeForce/1927B.cpp
+++ CodeForce/1927B.cpp
@@ -6,31 +6,34 @@ void solve(){
 
     map <char, int> m;
 
-    string s = "abcdefghijklmnopqrstuvwxyz";
-    int j = 0;
+    char ch = 'a';
+    string ans;
 
     for(int i=0; i<n; i++){
         int x; cin >> x;
 
         if(x == 0){
-            m.insert({s[j], 1});
-            cout << s[j];
-            j++;
+            m.insert({ch, 1});
+            ans.push_back(ch);
+            ch++;
         }
         else{
             for(auto &k : m){
                 if(k.second == x){
                     m[k.first]++;
-                    cout << k.first;
+                    ans.push_back(k.first);
                     break;
                 }
             }
         }
     }
-    cout << endl;
+    cout << ans << endl;
 }
 
 int main(){
+    ios::sync_with_stdio(0);
+	cin.tie(0);
+
     int t; cin >> t;
     while(t--){
         solve();
